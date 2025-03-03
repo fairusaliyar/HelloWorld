8dd7b5c8a0bbd51b1fca2530501b05079428b778
Gulfairus Aliyar <gulfairus.aliyar@gmail.com>
2025-03-03 09:49:40 +0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Gulfairus Aliyar on 03.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
